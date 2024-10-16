0f17e7becff44f6974c00a1600c4809d2ff2908e
raghveerdeol <deolraghveer5@gmail.com>
2024-10-16 18:35:37 +0200

    updated

--- highest and lowest/script.js
+++ highest and lowest/script.js
@@ -1,18 +1,16 @@
 // In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
-console.log(highAndLow('1 9 3 4 -5'));
+console.log(highAndLow(['8',  '3', '-5', '42',
+    '-1', '0', '0',  '-9',
+    '4',  '7', '4',  '-4']));
 function highAndLow(numbers){
-    let array = numbers.split(' ');
-    let min = array[0];
-    let max = array[0];
-    for (let i = 0; i < array.length; i++) {
-        if (min >= array[i]) {
-            min = array[i];
-        }
-        if (max <= array[i]) {
-            max = array[i]
-        }
+    let array = [];
+    if (Array.isArray(numbers)) {
+        array = numbers.map(Number);
+    } else {
+        array = numbers.split(' ');
     }
-    console.log(array)
+    let max = Math.max(...array);
+    let min = Math.min(...array);
     return  max +  " " + min;
 }
