cfc53944e18d3c5b9ce23e790e2a37df55e96b44
raghveerdeol <deolraghveer5@gmail.com>
2024-10-16 21:48:03 +0200

    did middleCharacter kata

---
+++ middleCharcter/script.js
@@ -0,0 +1,13 @@
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+console.log(getMiddle('middle'))
+function getMiddle(s)
+{
+    arrayS = s.trim().split('');
+    i = 0;
+    if (arrayS.length % 2 === 0) {
+        return arrayS[(arrayS.length / 2) - 1] + arrayS[(arrayS.length / 2)]; 
+    } else {
+        return arrayS[parseInt(arrayS.length / 2)];
+    }
+}
